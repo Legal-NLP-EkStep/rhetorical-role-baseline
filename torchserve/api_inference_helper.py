995d2f2131d7c4803c2590822bfdb7fce98847c1
amantiwari <amant5186@gmail.com>
2022-03-08 02:39:19 +0530

    fix

--- torchserve/api_inference_helper.py
+++ torchserve/api_inference_helper.py
@@ -12,8 +12,8 @@ def get_text_from_indiankanoon_url(url):
     return page_soup.text.strip()
 
 
-def get_predicted_rhetorical_roles(txt, inference_token):
-    rr_api_url = 'http://34.132.224.206:8080/predictions/RhetorcalRolePredictor/'
+def get_predicted_rhetorical_roles(ip, txt, inference_token):
+    rr_api_url = f'http://{ip}:8080/predictions/RhetorcalRolePredictor/'
     body = {'text': txt, 'inference_token': inference_token}
     req = urllib.request.Request(rr_api_url)
     req.add_header('Content-Type', 'application/json; charset=utf-8')
@@ -24,13 +24,16 @@ def get_predicted_rhetorical_roles(txt, inference_token):
     return json_data
 
 
-def get_rhetorical_roles_from_indiankanoon_url(ik_url, inference_token):
+def get_rhetorical_roles_from_indiankanoon_url(ik_url, inference_token, vm_ip_address):
     judgment_txt = get_text_from_indiankanoon_url(ik_url)
-    predicted_rr = get_predicted_rhetorical_roles(judgment_txt, inference_token)
+    predicted_rr = get_predicted_rhetorical_roles(judgment_txt, inference_token, ip=vm_ip_address)
     return predicted_rr
 
 
 if __name__ == "__main__":
     inference_token = '6977a1d9ff0e4c5cb285f210ddb4ff49'
+    vm_ip_address = ''
     output = get_rhetorical_roles_from_indiankanoon_url('https://indiankanoon.org/doc/137175626/',
-                                                        inference_token=inference_token)
+                                                        inference_token=inference_token, vm_ip_address=vm_ip_address)
+    with open("output.json", 'w') as f:
+        json.dump(output, f)
